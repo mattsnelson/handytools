d6ffb7e55e3b4edce3a45ec0ee59db86b0f49787
Matt MBP <matt_git@sent.com>
2020-03-18 15:22:02 +1100

    testing commit

---
+++ R/melt384.R
@@ -0,0 +1,18 @@
+#' Take a 384 well plate output and melt to tidy data
+#'
+#' Testing first as a simple function (takes 2 inputs)
+#'
+#' @param x this is the first number to input
+#' @param y this is the second number to input
+#'
+#'
+#'
+#'
+#'
+#'
+#' @export
+
+melt384 <- function(x,y=3){
+  x*2+y*2
+  Print("all done!")
+}
